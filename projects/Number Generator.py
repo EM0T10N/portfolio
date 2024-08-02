48e6be31d72f44215ec923b368e44700ec5f6513
EM0T10N <chloekellz5@gmail.com>
2024-08-02 19:41:59 +0100

    Number generator

---
+++ projects/Number Generator.py
@@ -0,0 +1,6 @@
+import random 
+
+number = random.randint(1,100)
+#random.randint will generate a random integer
+print("Random number generated: {}".format(number))
+#.format puts the variable into the curly braces.
